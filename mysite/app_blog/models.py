fadd4825171e2c0410ad29c9e9b3484b1bfeae89
Alex0V <alex2120032015@gmail.com>
2025-04-06 23:41:32 +0300

    update models

--- mysite/app_blog/models.py
+++ mysite/app_blog/models.py
@@ -6,10 +6,11 @@ from django.urls import reverse
 
 class Category(models.Model):
     category = models.CharField('Категорія', max_length=250, help_text='Максимум 250 символів')
-    
+    slug = models.SlugField('Слаг', unique=True, blank=True)
+
     class Meta:
-        verbose_name = 'Категорія для новини'
-        verbose_name_plural = 'Категорії для новин'
+        verbose_name = 'Категорія для публікації'
+        verbose_name_plural = 'Категорії для публікацій'
     
     def __str__(self):
         return self.category
@@ -19,8 +20,8 @@ class Article(models.Model):
     description = models.TextField(blank=True, verbose_name='Опис')
     pub_date = models.DateTimeField('Дата публікації', default=timezone.now)
     slug = models.SlugField('Слаг', unique_for_date='pub_date')
-    main_page = models.BooleanField('Головна', default=False, help_text='Показувати')
-    category = models.ForeignKey(Category, related_name='news', blank=True, null=True, verbose_name='Категорія', on_delete=models.CASCADE)
+    main_page = models.BooleanField('Головна', default=False, help_text='Показувати на головній сторінці')
+    category = models.ForeignKey(Category, related_name='articles', blank=True, null=True, verbose_name='Категорія', on_delete=models.CASCADE)
     objects = models.Manager()
 
     class Meta:
